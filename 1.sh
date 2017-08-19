f77fbf62b872c18153fe182c922ecb3d4e851c8d
dherath10 <dherath10@gmail.com>
2017-08-19 12:05:11 +0530

    upadte2

--- 1.sh
+++ 1.sh
@@ -1,2 +1,8 @@
 ls -l
 
+read -p "Enter a Number1:" n1
+
+read -p "Enter a Number2:" n2
+
+echo `expr $n1 + $n2`
+
---
+++ cal.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "Number 1 :" n1
+read -p "Number 2 :" n2
+
+read -p "Arithmatic Operator: (+ - * /)" ath 
+
+#when using expr there will be a prob with multification
+tot=$(($n1 $ath $n2))
+echo $tot
